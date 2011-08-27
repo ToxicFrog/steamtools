43131ba78491a5c565d8a10dab76cf341b4d4850
Ben Kelly <bkelly@uoguelph.ca>
2011-08-27 15:37:43 -0400

    Updated app.lua for better error reporting.

--- app.lua
+++ app.lua
@@ -1,7 +1,15 @@
-local r,e = xpcall(main, debug.traceback)
-if not r then
-    io.eprintf("\n\nAn error occurred! Please report this to the developer.\n%s\n", e)
+require "util.misc"
+
+local _main = main
+
+function main(...)
+    local r,e = va_xpcall(_main, debug.traceback, ...)
+    if not r then
+        io.eprintf("\n\nAn error occurred! Please report this to the developer.\n%s\n", e)
+    end
+    
+    io.printf("\nPress enter to quit...\n")
+    io.read()
+    os.exit(r and e or -1)
 end
 
-io.printf("\nPress enter to quit...\n")
-io.read()
