f47b58cd49d9593e876417aa4f4513f3b9f2d5e6
Ben Kelly <bkelly@uoguelph.ca>
2011-09-08 18:13:35 -0400

    Disable the pretty pictures on windows because I can't figure out how to get iup to load images from disk on windows.

--- bledit/fields.lua
+++ bledit/fields.lua
@@ -43,20 +43,19 @@ local fields = {
     original_system = SystemSelector();
     region = RegionSelector();
     ownership = {
-        iup.toggle { image = "images/own_owned.bmp" };
-        iup.toggle { image = "images/own_ghost.bmp" };
-        iup.toggle { image = "images/own_borrow.bmp" };
-        iup.toggle { image = "images/own_other.bmp" };
+        iup.toggle { image = LINUX and "images/own_owned.bmp" };
+        iup.toggle { image = LINUX and "images/own_ghost.bmp" };
+        iup.toggle { image = LINUX and "images/own_borrow.bmp" };
+        iup.toggle { image = LINUX and "images/own_other.bmp" };
     };
-        
     
     -- progress
     status = {
-        iup.toggle { image = "images/unfinished.bmp" };
-        iup.toggle { image = "images/beaten.bmp" };
-        iup.toggle { image = "images/completed.bmp" };
-        iup.toggle { image = "images/mastered.bmp" };
-        iup.toggle { image = "images/null.bmp" };
+        iup.toggle { image = LINUX and "images/unfinished.bmp" };
+        iup.toggle { image = LINUX and "images/beaten.bmp" };
+        iup.toggle { image = LINUX and "images/completed.bmp" };
+        iup.toggle { image = LINUX and "images/mastered.bmp" };
+        iup.toggle { image = LINUX and "images/null.bmp" };
     };
     achievements = iup.text { size = "30x"; spin = "YES"; spinmax = 999; };
     max_achievements = iup.text { size = "30x"; spin = "YES"; spinmax = 999; };
@@ -65,11 +64,11 @@ local fields = {
     
     -- review
     rating = {
-        iup.toggle { image = "images/5_5stars.bmp" };
-        iup.toggle { image = "images/4_5stars.bmp" };
-        iup.toggle { image = "images/3_5stars.bmp" };
-        iup.toggle { image = "images/2_5stars.bmp" };
-        iup.toggle { image = "images/1_5stars.bmp" };
+        iup.toggle { image = LINUX and "images/5_5stars.bmp"; title = LINUX and nil or "5 stars" };
+        iup.toggle { image = LINUX and "images/4_5stars.bmp"; title = LINUX and nil or "4 stars" };
+        iup.toggle { image = LINUX and "images/3_5stars.bmp"; title = LINUX and nil or "3 stars" };
+        iup.toggle { image = LINUX and "images/2_5stars.bmp"; title = LINUX and nil or "2 stars" };
+        iup.toggle { image = LINUX and "images/1_5stars.bmp"; title = LINUX and nil or "1 stars" };
         iup.toggle { title = "No Rating" };
         
         readmap = { 5, 4, 3, 2, 1, 0 };
