e11891305745232b1b7bdba821a04dd971299b15
Ben Kelly <bkelly@uoguelph.ca>
2011-10-02 13:46:35 -0400

    Support for categories in libsteam

--- libsteam.lua
+++ libsteam.lua
@@ -4,6 +4,7 @@ require "util.string"
 require "util.table"
 
 steam = {}
+require "libsteam.vdf"
 
 steam.__index = steam
 function steam:__tostring()
@@ -101,4 +102,68 @@ end
 -- return the URL of this user's Steam Community page
 function steam:community_url()
     return "http://steamcommunity.com/profiles/7656%.0f" % (self.Y + self.Z * 2 + 1197960265728)
+end
+
+function steam:config_path()
+    return self.path.."/userdata/"..(self.Z*2 + self.Y).."/7/remote/sharedconfig.vdf"
+end
+
+function steam:load_categories() 
+    local sharedconfig = assert(steam.loadVDF(self.path.."/userdata/"..(self.Z*2 + self.Y).."/7/remote/sharedconfig.vdf"))
+    
+    local apps
+    if sharedconfig.UserRoamingConfigStore then
+        apps = sharedconfig.UserRoamingConfigStore.Software.Valve.Steam.apps
+    elseif sharedconfig.UserLocalConfigStore then
+        apps = sharedconfig.UserLocalConfigStore.Software.Valve.Steam.apps
+    else
+        return error("Couldn't find User(Roaming|Local)ConfigStore in sharedconfig.vdf")
+    end
+    
+    for appid,game in pairs(self:games("appid")) do
+        local tags = (apps[tostring(appid)] and apps[tostring(appid)].tags)
+        if tags then
+            for k,v in pairs(tags) do
+                if v == "favorite" then
+                    game.favorite = true
+                else
+                    game.category = v
+                end
+            end
+        end
+    end
+    
+    return self:games()
+end
+
+function steam:save_categories()
+    -- extract the apps table from the VDF in the same manner as load_categories
+    local sharedconfig = assert(steam.loadVDF(self:config_path()))
+    
+    local apps
+    if sharedconfig.UserRoamingConfigStore then
+        apps = sharedconfig.UserRoamingConfigStore.Software.Valve.Steam.apps
+    elseif sharedconfig.UserLocalConfigStore then
+        apps = sharedconfig.UserLocalConfigStore.Software.Valve.Steam.apps
+    else
+        return error("Couldn't find User(Roaming|Local)ConfigStore in sharedconfig.vdf")
+    end
+    
+    -- now iterate over every game in the steam game list and update the 
+    -- category information for it
+    for appid,game in pairs(self:games("appid")) do
+        local app = apps[tostring(appid)] or {}
+        app.tags = {}
+        if game.favorite then
+            app.tags["0"] = "favorite"
+            app.tags["1"] = game.category
+        else
+            app.tags["0"] = game.category
+        end
+        apps[tostring(appid)] = app
+    end
+    
+    -- create a backup of the old VDF file, and save the new
+    io.writefile(self:config_path()..".backup."..tostring(os.time()), io.readfile(self:config_path()))
+    steam.saveVDF(self:config_path(), sharedconfig)
 end
