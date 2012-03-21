35ab885a2efcdc9fa6ae26dbe8d1bf03a87b8a86
Ben Kelly <bkelly@uoguelph.ca>
2012-03-21 01:00:13 -0400

    Windows binary update

--- mkwin32.sh
+++ mkwin32.sh
@@ -1,5 +1,7 @@
-enceladus -t ~/bin/enceladus.exe -o embed.exe steam2backloggery.lua app.lua libbl.lua libsteam.lua libsteam/vdf.lua util/* config.lua html.lua lib/*.lua lib/socket/*.lua
+LIBS="app.lua libbl.lua libsteam.lua libsteam/* xml/*.lua util/* config.lua html.lua lib/*.lua lib/socket/*.lua"
+
+enceladus -t ~/bin/enceladus.exe -o embed.exe steam2backloggery.lua app.lua $LIBS
 mv steam2backloggery.lua-embed.exe bin/steam2backloggery.exe
 
-enceladus -t ~/bin/enceladus.exe -o embed.exe categories.lua app.lua libsteam.lua libsteam/vdf.lua util/* config.lua html.lua lib/*.lua lib/socket/*.lua
+enceladus -t ~/bin/enceladus.exe -o embed.exe categories.lua $LIBS
 mv categories.lua-embed.exe bin/categories.exe
