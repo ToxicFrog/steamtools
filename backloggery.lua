2ad34fab83654ca5329f5b833f6a6da5643df52b
Ben Kelly <bkelly@uoguelph.ca>
2012-06-26 16:09:52 -0400

    List backloggery games

--- backloggery.lua
+++ backloggery.lua
@@ -1,9 +1,12 @@
 function main()
     require "libbl"
     require "util.init"
+    require "config"
+
+    CONFIG = config.load("steamtools.cfg")
     
-    local user = USER or io.prompt("Backloggery username: ")
-    local pass = PASS or io.prompt("Backloggery password: ")
+    local user = CONFIG.USER or io.prompt("Backloggery username: ")
+    local pass = CONFIG.PASS or io.prompt("Backloggery password: ")
     
     local cookie,err = bl.login(user,pass)
     
@@ -16,22 +19,18 @@ function main()
 
     io.printf("Loading Backloggery game lists:"); io.flush()
     io.printf(" games"); io.flush(); cookie:games()
-    io.printf(" wishlist"); io.flush(); cookie:wishlist()
     io.printf(" done.\n\n")
 
     local games = {}
     for _,game in pairs(cookie:games()) do
         games[#games+1] = game
     end
-    for _,game in pairs(cookie:wishlist()) do
-        games[#games+1] = game
-    end
     
     table.sort(games, L "x,y -> x.name < y.name")
     
     for _,game in ipairs(games) do
-        io.printf("%-10s%-14s%s\n%-24s%s\n", game.console, game.wishlist and "wishlist" or game.complete, game.name, tostring(game.id), game.note)
+        io.printf("%-10s%-14s%s\n%-24s%s\n", game.console, game.wishlist and "wishlist" or game.complete, game.name, tostring(game.id), game.note or "")
     end
 end
 
-require "app"
+require "app"; main(...)
